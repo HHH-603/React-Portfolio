c203865b50be9b8ccc9e84761aae4ae4d9aff2cf
Henry Hamilton Holmes <hholmes726@gmail.com>
2021-01-14 23:44:51 -0500

    Added my LinkedIn link.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
             <h1>Contact</h1>
             <h3>Email: hholmes726@gmail.com</h3>
             <h3>GitHub: <a href="https://github.com/HHH-603">https://github.com/HHH-603</a></h3>
+            <h3>Linkedin: <a href="https://www.linkedin.com/in/henryhamiltonholmes/">Visit my profile</a></h3>
         </div>
     )
 }
